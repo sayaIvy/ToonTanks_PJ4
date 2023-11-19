a29e1dd8e57247e94bf34e1d9121d588827b1e7a
sayaIvy <saya6414h@gmail.com>
2023-11-19 17:47:30 +0900

    [add] timer to delay start

--- Source/ToonTanks/ToonTanksGameMode.cpp
+++ Source/ToonTanks/ToonTanksGameMode.cpp
@@ -5,6 +5,9 @@
 #include "Kismet/GameplayStatics.h"
 #include "Tank.h"
 #include "Tower.h"
+#include "ToonTanksPlayerController.h"
+
+#include "TimerManager.h"
 
 
 void AToonTanksGameMode::ActorDied(AActor *DeadActor)
@@ -12,10 +15,9 @@ void AToonTanksGameMode::ActorDied(AActor *DeadActor)
   if(DeadActor == Tank)
   {
     Tank->HandleDestruction();
-    if(Tank->GetTankPlayerController())
+    if(ToonTanksPlayerController)
     {
-      Tank->DisableInput(Tank->GetTankPlayerController());
-      Tank->GetTankPlayerController()->bShowMouseCursor = false;
+      ToonTanksPlayerController->SetPlayerEnabledState(false);
     }
   }
   else if(ATower* DestroyedTower = Cast<ATower>(DeadActor))
@@ -23,12 +25,39 @@ void AToonTanksGameMode::ActorDied(AActor *DeadActor)
     DestroyedTower->HandleDestruction();
   }
 
+  FTimerDelegate TimerDelegate = FTimerDelegate::CreateUObject(this, &AToonTanksGameMode::BeginPlay);
 }
 
 void AToonTanksGameMode::BeginPlay()
 {
   Super::BeginPlay();
 
-  Tank = Cast<ATank>(UGameplayStatics::GetPlayerPawn(this, 0));
+  HandleGameStart();  
 }
 
+void AToonTanksGameMode::HandleGameStart()
+{
+  Tank = Cast<ATank>(UGameplayStatics::GetPlayerPawn(this, 0));
+  ToonTanksPlayerController = Cast<AToonTanksPlayerController>(UGameplayStatics::GetPlayerController(this, 0));
+
+  if(ToonTanksPlayerController)
+  {
+    ToonTanksPlayerController->SetPlayerEnabledState(false);
+
+    FTimerHandle PlayerEnableTimerHandle;
+    FTimerDelegate PlayerEnableTimerDelegate = FTimerDelegate::CreateUObject(
+      ToonTanksPlayerController,
+      &AToonTanksPlayerController::SetPlayerEnabledState,
+      true
+    );
+    GetWorldTimerManager().SetTimer(
+      PlayerEnableTimerHandle,
+      PlayerEnableTimerDelegate,
+      StartDelay,
+      false
+    );
+  }
+  
+
+  
+}
--- Source/ToonTanks/ToonTanksGameMode.h
+++ Source/ToonTanks/ToonTanksGameMode.h
@@ -25,5 +25,10 @@ protected:
 private:
 
 	class ATank* Tank;
+	class AToonTanksPlayerController* ToonTanksPlayerController;
+
+	float StartDelay = 3.f;
+
+	void HandleGameStart();
 	
 };
