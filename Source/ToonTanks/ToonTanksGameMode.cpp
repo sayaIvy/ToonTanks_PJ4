4c50e033d9cc8b2687a6a37ffe8ce5e078a40521
sayaIvy <saya6414h@gmail.com>
2023-11-19 23:56:10 +0900

    [add] Game win and lose test codes

--- Source/ToonTanks/ToonTanksGameMode.cpp
+++ Source/ToonTanks/ToonTanksGameMode.cpp
@@ -19,10 +19,16 @@ void AToonTanksGameMode::ActorDied(AActor *DeadActor)
     {
       ToonTanksPlayerController->SetPlayerEnabledState(false);
     }
+    GameOver(false);
   }
   else if(ATower* DestroyedTower = Cast<ATower>(DeadActor))
   {
     DestroyedTower->HandleDestruction();
+    --TargetTowers;
+    if(TargetTowers == 0)
+    {
+      GameOver(true);
+    }
   }
 
   FTimerDelegate TimerDelegate = FTimerDelegate::CreateUObject(this, &AToonTanksGameMode::BeginPlay);
@@ -37,6 +43,7 @@ void AToonTanksGameMode::BeginPlay()
 
 void AToonTanksGameMode::HandleGameStart()
 {
+  TargetTowers = GetTargetTowerCount();
   Tank = Cast<ATank>(UGameplayStatics::GetPlayerPawn(this, 0));
   ToonTanksPlayerController = Cast<AToonTanksPlayerController>(UGameplayStatics::GetPlayerController(this, 0));
 
@@ -59,7 +66,11 @@ void AToonTanksGameMode::HandleGameStart()
       false
     );
   }
-  
+}
 
-  
+int32 AToonTanksGameMode::GetTargetTowerCount()
+{
+  TArray<AActor*> Towers;
+  UGameplayStatics::GetAllActorsOfClass(this, ATower::StaticClass(), Towers);
+  return Towers.Num();
 }
